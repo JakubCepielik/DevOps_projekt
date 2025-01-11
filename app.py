1de08b58dab4d51cee76ac3cb028ba0900662415
Jakub Cepielik <jakubcepielik168@gmail.com>
2025-01-11 10:51:01 +0100

    change app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Witaj w mojej aplikacji Flask!"
+    return "Witaj kolego, piękna pogoda na spacer!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
